ca146d615ec528333b0826db1d300c4b3deae60a
jemmafaye13-hue <jemma.faye13@gmail.com>
2026-04-30 00:25:23 +0800

    Add Subtraction and Multiplication logic

--- main_program.py
+++ main_program.py
@@ -7,3 +7,12 @@ class Operation:
 class Addition(Operation):
     def calculate(self, num1, num2):
         return num1 + num2
+
+class Subtraction(Operation):
+    def calculate(self, num1, num2):
+        return num1 - num2
+
+class Multiplication(Operation):
+    def calculate(self, num1, num2):
+        return num1 * num2
+    
